235e53f9a7ef6afd25947a4fe75c296897671441
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2024-12-17 16:13:39 +0500

    Bot bootstrapping moved

--- lib/bodhi/tg_webhook_handler.ex
+++ lib/bodhi/tg_webhook_handler.ex
@@ -8,7 +8,9 @@ defmodule Bodhi.TgWebhookHandler do
   def on_boot() do
     # env_config = Application.get_env(:bodhi, __MODULE__)
     # delete the webhook and set it again
-    {:ok, true} = Telegex.delete_webhook()
+    #{:ok, true} = Telegex.delete_webhook()
+    #{:ok, bot_user} = Telegex.get_me()
+    #Bodhi.Users.create_or_update_user(bot_user)
     # set the webhook (url is required)
     # {:ok, true} = Telegex.set_webhook(env_config[:webhook_url])
     # specify port for web server
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,5 +10,3 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-{:ok, bot_user} = Telegex.get_me()
-Bodhi.Users.create_or_update_user(bot_user)
